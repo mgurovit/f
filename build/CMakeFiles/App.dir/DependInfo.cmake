9c98d78a1c060d17f85374af249cb891506b1df6
mgurovit <mgurovit@ucsc.edu>
2025-03-20 06:30:42 -0700

    w

---
+++ build/CMakeFiles/App.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/michelle/fake/f/debugging/app.cpp" "CMakeFiles/App.dir/debugging/app.cpp.o" "gcc" "CMakeFiles/App.dir/debugging/app.cpp.o.d"
+  "/home/michelle/fake/f/debugging/main.cpp" "CMakeFiles/App.dir/debugging/main.cpp.o" "gcc" "CMakeFiles/App.dir/debugging/main.cpp.o.d"
+  "/home/michelle/fake/f/debugging/menu.cpp" "CMakeFiles/App.dir/debugging/menu.cpp.o" "gcc" "CMakeFiles/App.dir/debugging/menu.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/App.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "App"
+  "App.pdb"
+  "CMakeFiles/App.dir/debugging/app.cpp.o"
+  "CMakeFiles/App.dir/debugging/app.cpp.o.d"
+  "CMakeFiles/App.dir/debugging/main.cpp.o"
+  "CMakeFiles/App.dir/debugging/main.cpp.o.d"
+  "CMakeFiles/App.dir/debugging/menu.cpp.o"
+  "CMakeFiles/App.dir/debugging/menu.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/App.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
